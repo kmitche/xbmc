080e6fd37bd4a095912733d3a9cc39a234890728
David Teirney <david@teirney.net>
2011-01-31 23:35:11 +1300

    Refactor: remove unused parental_rating variable that isn't supported by the existing MythTV interfaces.

--- xbmc/pvrclients/mythtv/libmythxml/SEpg.h
+++ xbmc/pvrclients/mythtv/libmythxml/SEpg.h
@@ -16,13 +16,11 @@ struct SEpg
   time_t      end;
   int         genre_type; // TODO: use category and do the mapping to genre in the client interface.
   int         genre_subtype;
-  int         parental_rating; // TODO: remove as not available through MythXML.
 
   SEpg() {
     chanid          = 0;
     channum        = 0;
     genre_type      = 0;
     genre_subtype   = 0;
-    parental_rating = 0;
   }
 };
